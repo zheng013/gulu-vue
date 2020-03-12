bc86dff078625de2473f808bb48e4e0b38eff6d8
zheng <849859182@qq.com>
2020-03-12 10:20:11 +0800

    完成对于buttonGround子元素不是button的检测

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -12,34 +12,49 @@
   @Component
   export default class ButtonGroup extends Vue {
     name: "gButtonGroup";
+
+    mounted() {
+      let nodes = this.$el.children;
+      for (let node of nodes) {
+        const name = node.nodeName.toLowerCase();
+        if (name !== "button") {
+        console.warn(`该组件的子节点必须为button元素，而您输入的是${name}`)
+        }
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-.g-button-group{
-    display: inline-flex;
-    vertical-align: middle;
-    >.g-button{
-        border-radius: 0;
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
 
-        &:nth-child(2){
-            margin-left: -1px;
-        }
-        &:nth-child(3){
-            margin-left: -2px;
-        }
-        &:first-child{
-            border-top-left-radius: var(--border-radius);
-            border-bottom-left-radius: var(--border-radius);
-        }
-        &:last-child{
-            border-top-right-radius: var(--border-radius);
-            border-bottom-right-radius: var(--border-radius);
-        }
-        &:hover{
-            position: relative;
-            z-index: 1;
+        > .g-button {
+            border-radius: 0;
+
+            &:nth-child(2) {
+                margin-left: -1px;
+            }
+
+            &:nth-child(3) {
+                margin-left: -2px;
+            }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
         }
     }
-}
 </style>
