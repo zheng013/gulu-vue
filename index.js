2694a5689922c2492dce0b08c203fc6118c08028
zheng <849859182@qq.com>
2020-03-12 20:39:20 +0800

    新建一个入口文件index.js，修改包的的名称。发布到npm上。

---
+++ index.js
@@ -0,0 +1,4 @@
+import Button from 'src/Button.vue'
+import ButtonGroup from 'src/ButtonGroup.vue'
+import Icon from 'src/Icon.vue'
+export {Button,ButtonGroup,Icon}
--- test/button.test.js
+++ test/button.test.js
@@ -65,3 +65,4 @@ describe('Button', () => {
 
   })
 })
+
