7def52b6bc092886e6ca135a486d1edc218eef52
zheng <849859182@qq.com>
2020-03-16 21:40:55 +0800

    利用sass的变量和@for的loop属性，实现平均分配的布局{[``]:}  。

--- src/Col.vue
+++ src/Col.vue
@@ -1,30 +1,37 @@
 <template>
-    <div class="col">
+    <div class="col" :class="{[`col-${span}`]:span,[`offset-${offset}`]:offset}">
         <slot></slot>
     </div>
 </template>
 
 <script lang="ts">
   import Vue from "vue";
-  import {Component} from "vue-property-decorator";
+  import {Component, Prop} from "vue-property-decorator";
 
   @Component
   export default class Col extends Vue {
-
+    @Prop({type: [String, Number]}) span: string | number;
+    @Prop({type: [String, Number]}) offset: string | number;
   }
 </script>
 
 <style lang="scss" scoped>
-.col{
-    width: 50%;
-    height:100px;
-    background: #999;
-    border: 1px solid darkred;
-    $class-prefix:col-;
-    @for $n from 1 through 24{
-    &.#{$class-prefix}#{$n}{
-        width: ($n/24)*100%;
+    .col {
+        width: 50%;
+        height: 100px;
+        background: #999;
+        border: 1px solid darkred;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
+            }
+        }
     }
-}
-}
 </style>
